69fe6d3111148a131bb0527b186aadd5350d3d6b
jorge-flp <jorgefeliperodrigues841@gmail.com>
2026-07-27 15:02:24 -0300

    criando menu hamburguer

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -24,3 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
         observer.observe(item);
     });
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
